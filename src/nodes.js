0c54a094376c4876ebd8e2757eeb5cf3c86ef2e9
Mateusz Kadlubowski <xeho91@pm.me>
2024-12-09 00:36:56 +0800

    remove types which are now exportable via PR

--- src/nodes.js
+++ src/nodes.js
@@ -1,21 +1,9 @@
 /**
  * @import * as _reset from "@total-typescript/ts-reset";
  * @import { Node as ESTreeNode } from "estree";
- * @import { AST, Css, parse } from "svelte/compiler";
+ * @import { AST, parse } from "svelte/compiler";
  */
 
-/**
- * TODO: Svelte could expose this type to public too: https://github.com/sveltejs/svelte/blob/2b362ddc63e930726b67a1010d2829a3a4f6bb85/packages/svelte/src/compiler/types/template.d.ts#L514
- * FIXME: Include `Css.Node` when exposed to public
- * @typedef {ESTreeNode | TemplateNode | AST.Fragment} SvelteNode
- */
-
-/**
- * TODO: Svelte could expose this type to public too: https://github.com/sveltejs/svelte/blob/2b362ddc63e930726b67a1010d2829a3a4f6bb85/packages/svelte/src/compiler/types/template.d.ts#L476
- * @typedef {AST.AnimateDirective | AST.BindDirective | AST.ClassDirective | AST.LetDirective | AST.OnDirective | AST.SpreadAttribute | AST.StyleDirective | AST.TransitionDirective | AST.UseDirective} Directive
- */
-
-/** @typedef {AST.Attribute | AST.SpreadAttribute | Directive} AttributeLike */
 const ATTRIBUTE_LIKE_NODE_NAMES = new Set(
 	/** @type {const} */ ([
 		"AnimateDirective",
@@ -32,7 +20,7 @@ const ATTRIBUTE_LIKE_NODE_NAMES = new Set(
 );
 
 /**
- * Type check guard to see if provided AST node is {@link AttributeLike}.
+ * Type check guard to see if provided AST node is {@link AST.AttributeLike}.
  *
  * - Standard attribute _({@link AST.Attribute})_ - {@link https://developer.mozilla.org/en-US/docs/Glossary/Attribute}
  * - Spread attribute _({@link AST.SpreadAttribute})_ - {@link https://svelte.dev/docs/basic-markup#attributes-and-props}
@@ -41,14 +29,9 @@ const ATTRIBUTE_LIKE_NODE_NAMES = new Set(
  *   - element - {@link https://svelte.dev/docs/element-directives}
  *
  * @param {SvelteNode} node - Supported AST node to narrow down its inferred type
- * @returns {node is AttributeLike}
+ * @returns {node is AST.AttributeLike}
  */
 export const is_attribute_like_node = (node) => ATTRIBUTE_LIKE_NODE_NAMES.has(node.type);
-
-/**
- * TODO: Svelte could expose this type to public too: https://github.com/sveltejs/svelte/blob/2b362ddc63e930726b67a1010d2829a3a4f6bb85/packages/svelte/src/compiler/types/template.d.ts#L486
- * @typedef {AST.EachBlock | AST.IfBlock | AST.AwaitBlock | AST.KeyBlock | AST.SnippetBlock} Block
- */
 
 const BLOCK_NODE_NAMES = new Set(
 	/** @type {const} */ ([
@@ -67,11 +50,8 @@ const BLOCK_NODE_NAMES = new Set(
  *
  * @see {@link https://svelte.dev/docs/logic-blocks}
  *
- * NOTE: Svelte v5 includes a new block {@link AST.SnippetBlock}
- * @see {@link https://svelte-5-preview.vercel.app/docs/snippets}
- *
  * @param {SvelteNode} node - Supported AST node to narrow down its inferred type
- * @returns {node is Block}
+ * @returns {node is AST.Block}
  */
 export const is_block_node = (node) => BLOCK_NODE_NAMES.has(node.type);
 
@@ -98,19 +78,14 @@ const CSS_AST_NODE_NAMES = new Set(
 	]),
 );
 /**
- * FIXME: `Css` is not exported
- * Type check guard to see if provided AST node is a CSS based {@link Css.Node}.
+ * Type check guard to see if provided AST node is a CSS based {@link AST.Css.Node}.
  *
  * WARN: Good to know: they're not same _(complaint)_ with `css-tree`!
  *
- * @param {SvelteNode} node - Supported AST node to narrow down its inferred type
+ * @param {AST.SvelteNode} node - Supported AST node to narrow down its inferred type
  * @returns {node is AST.Css.Node}
  */
 export const is_css_node = (node) => CSS_AST_NODE_NAMES.has(node.type);
-
-/**
- * TODO: Svelte could expose this type to public too: https://github.com/sveltejs/svelte/blob/2b362ddc63e930726b67a1010d2829a3a4f6bb85/packages/svelte/src/compiler/types/template.d.ts#L488
- * @typedef {AST.Component | AST.TitleElement | AST.SlotElement | AST.RegularElement | AST.SvelteBody | AST.SvelteComponent | AST.SvelteDocument | AST.SvelteElement | AST.SvelteFragment | AST.SvelteHead | AST.SvelteOptionsRaw | AST.SvelteSelf | AST.SvelteWindow} ElementLike */
 
 const ELEMENT_LIKE_NODE_NAMES = new Set(
 	/** @type {const} */ ([
@@ -118,6 +93,7 @@ const ELEMENT_LIKE_NODE_NAMES = new Set(
 		"RegularElement",
 		"SlotElement",
 		"SvelteBody",
+		"SvelteBoundary",
 		"SvelteComponent",
 		"SvelteDocument",
 		"SvelteElement",
@@ -130,7 +106,7 @@ const ELEMENT_LIKE_NODE_NAMES = new Set(
 	]),
 );
 /**
- * Type check guard to see if provided AST node is "element-like" {@link ElementLike}.
+ * Type check guard to see if provided AST node is "element-like" {@link AST.ElementLike}.
  *
  * Those are:
  *
@@ -138,8 +114,8 @@ const ELEMENT_LIKE_NODE_NAMES = new Set(
  * - regular element _(HTML based)_ - {@link https://developer.mozilla.org/en-US/docs/Web/HTML/Element}
  * - special Svelte elements - {@link https://svelte.dev/docs/special-elements}
  *
- * @param {SvelteNode} node - Supported AST node to narrow down its inferred type
- * @returns {node is ElementLike}
+ * @param {AST.SvelteNode} node - Supported AST node to narrow down its inferred type
+ * @returns {node is AST.ElementLike}
  */
 export const is_element_like_node = (node) => ELEMENT_LIKE_NODE_NAMES.has(node.type);
 
@@ -177,12 +153,7 @@ const TAG_NODE_NAMES = new Set(
 );
 
 /**
- * TODO: Svelte could expose this type to public too: https://github.com/sveltejs/svelte/blob/2b362ddc63e930726b67a1010d2829a3a4f6bb85/packages/svelte/src/compiler/types/template.d.ts#L474
- * @typedef {AST.ExpressionTag | AST.HtmlTag | AST.ConstTag | AST.DebugTag | AST.RenderTag} Tag
- */
-
-/**
- * Type check guard to see if provided AST node is "tag-like" {@link Tag}.
+ * Type check guard to see if provided AST node is "tag-like" {@link AST.Tag}.
  *
  * @see {@link https://svelte.dev/docs/special-tags}
  *
@@ -192,17 +163,12 @@ const TAG_NODE_NAMES = new Set(
  * - render tag _({@link AST.RenderTag})_ - part of Svelte v5
  *
  * @param {SvelteNode} node - Supported AST node to narrow down its inferred type
- * @returns {node is Tag}
+ * @returns {node is AST.Tag}
  */
 export const is_tag_node = (node) => TAG_NODE_NAMES.has(node.type);
 
 /**
- * TODO: Svelte could expose this type to public too: https://github.com/sveltejs/svelte/blob/2b362ddc63e930726b67a1010d2829a3a4f6bb85/packages/svelte/src/compiler/types/template.d.ts#L503
- * @typedef {AST.Root | AST.Text | Tag | ElementLike | AST.Attribute | AST.SpreadAttribute | Directive | AST.Comment | Block} TemplateNode
- */
-
-/**
- * Type check guard to see if provided AST node is part of node used for templating {@link TemplateNode}.
+ * Type check guard to see if provided AST node is part of node used for templating {@link AST.TemplateNode}.
  *
  * Those are:
  *
@@ -210,8 +176,8 @@ export const is_tag_node = (node) => TAG_NODE_NAMES.has(node.type);
  * - text that is included between HTML tags - {@link AST.Text}
  * - HTML comment - {@link AST.Comment}
  *
- * @param {SvelteNode} node - Supported AST node to narrow down its inferred type
- * @returns {node is TemplateNode}
+ * @param {AST.SvelteNode} node - Supported AST node to narrow down its inferred type
+ * @returns {node is AST.TemplateNode}
  */
 export const is_template_node = (node) =>
 	node.type === "Root" ||
@@ -228,7 +194,7 @@ export const is_template_node = (node) =>
  * Not all of the nodes are bundled together with {@link AST.BaseNode}.
  * This type wraps them together as supported ones for printing.
  *
- * @typedef {AST.Script | AST.BaseNode | AST.SvelteOptionsRaw | SvelteNode} SupportedSvelteNode
+ * @typedef {AST.Script | AST.BaseNode | AST.SvelteOptionsRaw | AST.SvelteNode} SupportedSvelteNode
  */
 
 /**
